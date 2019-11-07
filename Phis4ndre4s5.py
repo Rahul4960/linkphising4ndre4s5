e1c8a49335b7c52f0c83c5943b3d209ac5bda86d
Rahul4960 <57430177+Rahul4960@users.noreply.github.com>
2019-11-07 12:22:30 +0800

    Update and rename weeman.py to Phis4ndre4s5.py

--- Phis4ndre4s5.py
+++ Phis4ndre4s5.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python2
 #
-# weeman.py - HTTP server for phishing
+# Phis4ndre4s5.py - HTTP server for phishing
 #
-#  Weeman is free software; you can redistribute it and/or modify
+#  Phis4ndre4s5 is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  Weeman is distributed in the hope that it will be useful,
+#  Phis4ndre4s5 is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
